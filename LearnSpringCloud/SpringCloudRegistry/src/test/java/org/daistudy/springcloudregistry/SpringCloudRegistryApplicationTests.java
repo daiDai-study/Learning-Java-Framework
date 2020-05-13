42a74c6b85b876b4caef68d951b819cea2911b67
Chen Zhen2 <chenzhen2@aactechnologies.com>
2020-05-13 19:36:10 +0800

    SpringCloud 学习基本概念、注册中心

---
+++ LearnSpringCloud/SpringCloudRegistry/src/test/java/org/daistudy/springcloudregistry/SpringCloudRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daistudy.springcloudregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
